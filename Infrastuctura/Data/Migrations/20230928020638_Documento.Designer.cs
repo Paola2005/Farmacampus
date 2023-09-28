a09ff5ebd270debf38b3161efe37f01b3ab25264
Paola2005 <137318847+Paola2005@users.noreply.github.com>
2023-09-27 21:25:19 -0500

    feat: :sparkles:

--- Infrastuctura/Data/Migrations/20230928020638_Documento.Designer.cs
+++ Infrastuctura/Data/Migrations/20230928020638_Documento.Designer.cs
@@ -2,6 +2,7 @@
 using Infrastuctura.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastuctura.Data.Migrations
 {
     [DbContext(typeof(TiendaCampusContext))]
-    partial class TiendaCampusContextModelSnapshot : ModelSnapshot
+    [Migration("20230928020638_Documento")]
+    partial class Documento
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Infrastuctura/Repositories/PaisRepository.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+
+using Infrastuctura.Data;
+
+namespace Infrastructure.Repositories;
+
+public class PaisRepository : GenericRepository<Pais>
+{
+    private readonly TiendaCampusContext _context;
+
+    public PaisRepository(TiendaCampusContext context) : base(context)
+    {
+        _context = context;
+    }
+}
